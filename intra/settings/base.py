164b09ef66febbbb0bc7a3a9c3c6a7f48ba58323
Josmer Hernandez <jhernandez.18p@gmail.com>
2016-08-18 17:38:28 -0500

    base

--- intra/settings/base.py
+++ intra/settings/base.py
@@ -118,10 +118,6 @@ AUTHENTICATION_BACKENDS =(
                             'django.contrib.auth.backends.ModelBackend',
                             'local_apps.profiles.EmailBackend.EmailBackend',
                         )
-if DEBUG == True:
-    DJANGO_SETTINGS_MODULE = 'intra.settings.base'
-else:
-    DJANGO_SETTINGS_MODULE = 'intra.settings.production'
 
 LOGIN_URL = '/entrar/'
 LOGIN_REDIRECT_URL = '/'
