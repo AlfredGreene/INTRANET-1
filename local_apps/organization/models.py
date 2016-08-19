e1e1b8e95cb991881cc72cbbc3f6f086095b72dc
Josmer Hernandez <jhernandez.18p@gmail.com>
2016-08-19 13:29:48 -0500

    Organizations

--- local_apps/organization/models.py
+++ local_apps/organization/models.py
@@ -1,5 +1,56 @@
 from django.db import models
 
+from local_apps.brands.models import Brand
+
+class Location(models.Model):
+
+    COUNTRIES = (
+        (1,('Argentina')),
+        (2,('Bolivia')),
+        (3,('Brasil')),
+        (4,('Chile')),
+        (5,('Colombia')),
+        (6,('Costa Rica')),
+        (7,('Cuba')),
+        (8,('Ecuador')),
+        (9,('El Salvador')),
+        (10,('Guayana Francesa')),
+        (11,('Granada')),
+        (12,('Guatemala')),
+        (13,('Guayana')),
+        (14,('Haití')),
+        (15,('Honduras')),
+        (16,('Jamaica')),
+        (17,('México')),
+        (18,('Nicaragua')),
+        (19,('Paraguay')),
+        (20,('Panamá')),
+        (21,('Perú')),
+        (22,('Puerto Rico')),
+        (23,('República Dominicana')),
+        (24,('Surinam')),
+        (25,('Uruguay') ),
+        (26,('Venezuela')),
+        (27,('Estados Unidos')),
+    )
+
+    name = models.CharField(max_length=144)
+    dir1 = models.TextField(max_length=300)
+    dir2 = models.TextField(max_length=300, blank=True)
+    country = models.IntegerField(choices=COUNTRIES)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+
+        verbose_name = ('Location')
+        verbose_name_plural = ('Locations')
+        permissions = (
+            ("can_create_location", "Can create location"),
+            ("can_delete_location", "Can delete location"),
+            ("can_update_location", "Can update location"),
+        )
 
 class Organization(models.Model):
 
@@ -18,4 +69,24 @@ class Organization(models.Model):
             ("can_create_organization", "Can create organization"),
             ("can_delete_organization", "Can delete organization"),
             ("can_update_organization", "Can update organization"),
+        )
+
+
+class Store(models.Model):
+
+    name = models.CharField(max_length=50)
+    organization = models.ForeignKey(Organization)
+    location = models.ForeignKey(Location)
+    brand = models.ForeignKey(Brand)
+
+    def __srt__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = ('Store')
+        verbose_name_plural = ('Stores')
+        permissions = (
+            ("can_create_store", "Can create store"),
+            ("can_delete_store", "Can delete store"),
+            ("can_update_store", "Can update store"),
         )
