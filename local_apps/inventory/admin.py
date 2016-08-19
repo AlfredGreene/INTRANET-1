6de9eed3cf1c52eb86c550fc0a69a4d631bea053
Josmer Hernandez <jhernandez.18p@gmail.com>
2016-08-19 17:38:21 -0500

    inventory

--- local_apps/inventory/admin.py
+++ local_apps/inventory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+@admin.register(SO)
+class SOAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    list_display = ('name',)
+
+@admin.register(Red_Interface)
+class Red_InterfaceAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    list_display = ('name',)
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    fields = ('name','ws_type','usb_ports','red_interface','lan','wlan','ram','cpu','hdd','so','brand','model','version','user','user_pass','sn','employee','organization','status',)
+    list_display = ('name','ws_type','usb_ports','red_interface','lan','wlan','ram','cpu','hdd','so','brand','model','version','user','user_pass','sn','employee','organization','status',)
--- local_apps/inventory/models.py
+++ local_apps/inventory/models.py
@@ -1,29 +1,88 @@
 from django.db import models
 
-# Create your models here.
+from local_apps.brands.models import Brand
+from local_apps.profiles.models import Employee
+from local_apps.organization.models import Organization
+
+class Red_Interface(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    def __srt__(self):
+
+        return self.name
+
+    class Meta:
+
+        verbose_name = ('Red Interface')
+        verbose_name_plural = ('Red Interfaces')
+        permissions = (
+            ("can_create_red_interface", "Can create red interface"),
+            ("can_delete_red_interface", "Can delete red interface"),
+            ("can_update_red_interface", "Can update red interface"),
+        )
+
+class SO(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    def __srt__(self):
+
+        return self.name
+
+    class Meta:
+
+        verbose_name = ('Red SO')
+        verbose_name_plural = ('Red SO')
+        permissions = (
+            ("can_create_SO", "Can create SO"),
+            ("can_delete_SO", "Can delete SO"),
+            ("can_update_SO", "Can update SO"),
+        )
 
 class Equipment(models.Model):
 
     WORKSTATION_CHOICE_FIELDS = (
         (1, ('PC')),
-        (2, ('LAPTOP')),
-        (3, ('LAPTOP')),
-        (4, ('LAPTOP')),
-        (5, ('LAPTOP')),
-        (6, ('LAPTOP')),
-        (7, ('LAPTOP')),
-        (8, ('LAPTOP')),
-        (9, ('LAPTOP')),
-        (10, ('LAPTOP')),
-        (11, ('LAPTOP')),
-        (12, ('LAPTOP')),
-        (13, ('LAPTOP')),
-        (14, ('LAPTOP')),
-        (15, ('LAPTOP')),
+        (2, ('Laptop')),
+        (3, ('Teléfono')),
+        (4, ('Router')),
+        (5, ('Server')),
+        (6, ('Lector biométrico')),
+        (7, ('Batería de respaldo "Apc-upc"')),
+        (8, ('Impresora Fiscal')),
+        (9, ('Impresora multifuncional')),
+        (10, ('Punto de venta')),
+        (11, ('Lector de códigos de barra')),
+        (12, ('DVD')),
+        (13, ('Impresora zebra')),
+    )
+
+    STATUS_CHOICE_FIELDS = (
+        (1,('Activo')),
+        (2,('Dañao')),
     )
 
     name = models.CharField(max_length=50)
     ws_type = models.IntegerField(choices=WORKSTATION_CHOICE_FIELDS, default=1)
+    usb_ports = models.IntegerField(default=6, blank=True)
+    red_interface = models.ForeignKey(Red_Interface, blank=True)
+    lan = models.BooleanField(default=0, blank=True)
+    wlan = models.BooleanField(default=0, blank=True)
+    ram = models.IntegerField(default=2048, blank=True)
+    cpu = models.TextField(max_length=500, blank=True)
+    hdd = models.TextField(max_length=500, blank=True)
+    so = models.ForeignKey(SO)
+    brand = models.ForeignKey(Brand)
+    model = models.CharField(max_length=144, blank=True)
+    version = models.CharField(max_length=144, blank=True)
+    user = models.CharField(max_length=144, blank=True)
+    user_pass = models.CharField(max_length=144, blank=True)
+    sn = models.CharField(max_length=144, blank=True)
+    employee = models.ForeignKey(Employee, blank=True)
+    organization = models.ForeignKey(Organization, blank=True)
+    status = models.IntegerField(choices=STATUS_CHOICE_FIELDS, default=1)
+
 
     def __str__(self):
 
@@ -34,7 +93,7 @@ class Equipment(models.Model):
         verbose_name = ('Equipment')
         verbose_name_plural = ('Equipments')
         permissions = (
-            ("can_create_workstation", "Can create workstation"),
-            ("can_delete_workstation", "Can delete workstation"),
-            ("can_update_workstation", "Can update workstation"),
+            ("can_create_equipment", "Can create Equipment"),
+            ("can_delete_equipment", "Can delete Equipment"),
+            ("can_update_equipment", "Can update Equipment"),
         )
