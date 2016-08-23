1d811c9017e388941e32678831834b5ffeba40f8
Josmer Hernandez <jhernandez.18p@gmail.com>
2016-08-23 17:06:42 -0500

    auth

--- local_apps/profiles/auth.py
+++ local_apps/profiles/auth.py
@@ -45,7 +45,7 @@ def register(request):
 
 	if request.method == 'GET':
 
-		return render(request, 'auth/register.html',{'title':'Error al Registrarse'})
+		return render(request, 'auth/register.html',{'title':'Registro'})
 
 	elif request.method == 'POST' :
 
@@ -65,5 +65,5 @@ def register(request):
 		            [config("INTRA_EMAIL_HOST_USER",)],
 		            fail_silently=False,
 		        )
-				
+
 		return render(request, 'auth/registered.html',{'title':'registro satisfactorio'})
