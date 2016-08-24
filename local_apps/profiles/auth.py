830a651897900ca0b99f5e147c989cd79d5c2213
Josmer Hernandez <jhernandez.18p@gmail.com>
2016-08-24 12:47:53 -0500

    auth

--- local_apps/profiles/auth.py
+++ local_apps/profiles/auth.py
@@ -63,7 +63,7 @@ def register(request):
 				'title':'Error de Registro',
 				'error':{
 					'error':'show',
-					'email':'Su correo debe terminar en "@phoenixwt.com.pa"',
+					'email':'Su correo debe "%s" no terminar en "@phoenixwt.com.pa"' % email,
 					'contacto':'Por favor contactar al administrador de sistemas, soporte@phoenixwt.com.pa'
 				}
 			})
