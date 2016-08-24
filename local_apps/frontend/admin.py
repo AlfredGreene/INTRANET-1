5f6e8f6a7a092710ae7eabaacdc53d3d39177c49
Josmer Hernandez <jhernandez.18p@gmail.com>
2016-08-24 13:55:14 -0500

    model frontend

--- local_apps/frontend/admin.py
+++ local_apps/frontend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Documentation)
+class DocumentationAdmin(admin.ModelAdmin):
+    fields = ('author','name','title','description','document')
+    list_display = ('author','name','title','description','document')
--- local_apps/frontend/models.py
+++ local_apps/frontend/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Documentation(models.Model):
+
+    author = models.ForeignKey(User)
+    name = models.CharField(max_length=140)
+    title = models.CharField(max_length=140, blank=True)
+    description = models.TextField(blank=True)
+    document = models.FileField(upload_to='documents/')
+
+    def __str__(self):
+
+        return self.name
+
+    class Meta:
+
+        verbose_name = ('Documentation')
+        verbose_name_plural = ('Documentation')
+        permissions = (
+            ("can_add_documets", "Can add documents"),
+            ("can_delete_documents", "Can delete documents"),
+            ("can_see_finanzas_documents", "Can see finanzas documents"),
+            ("can_see_it_documents", "Can see it documents"),
+        )
