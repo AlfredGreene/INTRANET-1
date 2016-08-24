1f958845b58e7ed513a8ece6cb09b47c7e5524ad
Josmer Hernandez <jhernandez.18p@gmail.com>
2016-08-24 13:20:51 -0500

    url
    reportes

--- local_apps/reports/urls.py
+++ local_apps/reports/urls.py
@@ -6,6 +6,6 @@ from local_apps.reports import views as reports_views
 urlpatterns = [
     url(r'^$', reports_views.report, name="Report"),
     url(r'^topshop$', reports_views.topshop_reports, name="Topshop"),
-    url(r'^topshop/bwt', reports_views.report_bwt, name="TopshopBWT"),
-    url(r'^topshop/json/bwt', reports_views.json_report_bwt, name="TopshopJsonBWT"),
+    url(r'^topshop/bwt$', reports_views.report_bwt, name="TopshopBWT"),
+    url(r'^topshop/json/bwt$', reports_views.json_report_bwt, name="TopshopJsonBWT"),
 ]
